06f22bf25d46c2bfa3baf5a2f0adcd8a0d1ed6b7
Marcelo <marcelosilva@Marcelo-Silvas-Mac.local>
2010-10-22 16:06:39 -0200

    Iniciando o serviço de votos

--- twittface/urls.py
+++ twittface/urls.py
@@ -1,6 +1,8 @@
 from torneira.core.dispatcher import url
 from twittface.controller.home import HomeController
+from twittface.controller.voto import VotoController
 
 urls = (
     url("/", HomeController, action="", name="home"),    
+    url("/voto", VotoController, action="", name=""),
 )
