d599e93e412148786499e7621ece0488a68e5a29
marcel <marcel@corp.globo.com>
2010-10-22 17:41:04 -0200

    login callback

--- twittface/urls.py
+++ twittface/urls.py
@@ -1,11 +1,12 @@
 from torneira.core.dispatcher import url
 from twittface.controller.home import HomeController
-<<<<<<< HEAD
+from twittface.controller.voto import VotoController
 from twittface.controller.login import LoginController
 
 urls = (
     url("/", HomeController, action="", name="home"),    
     url("/voto", VotoController, action="", name=""),
     url("/login", LoginController, action="index", name="login_index"),    
+    url("/login/oauth", LoginController, action="oauth", name="login_oauth"),    
     url("/login/oauth_callback", LoginController, action="oauth_callback", name="login_oauth_callback"),    
 )
