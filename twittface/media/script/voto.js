bcd556101454a5b5765a4e155561b6f3280e3ff5
Marcelo <marcelosilva@Marcelo-Silvas-Mac.local>
2010-10-23 18:37:18 -0200

    mais js

--- twittface/media/script/voto.js
+++ twittface/media/script/voto.js
@@ -1,8 +1,35 @@
 $(document).ready( function(){
+	
+	TARGET_LIST = [];
+	
+	refresh_target = function(object_user){
+		
+		alert(object_user);
+		
+	},
+	
+	$("div.messageBox").bind("bota_e_tira", function(event, args){
+		console.log(args)
+		alert(args["msg"]);
+		return false;
+	});
+	
+	
+	$("div#banner", $("div.container")).bind("next_pic", function(event){
+		
+		if(TARGET_LIST.length == 0){
+			$.getJSON('/random_target', function(data) {
+				TARGET_LIST = data;
+				refresh_target(TARGET_LIST[0]);
+			});
+		}else{
+			refresh_target(TARGET_LIST[0]);
+		}
+		
+	});
 
 
 	$("a", $("div.ratings-num")).bind("click", function(){
-		
 		var nota = $(this).attr("id");
 		var eleitor = $("input[name='eleitor']").val();
 		var candidato = $("input[name='candidato']").val();
@@ -13,17 +40,18 @@ $(document).ready( function(){
 		  url: url,
 		  data: { "nota": nota, "eleitor": eleitor, "candidato": candidato  },
 		  success: function(data){
-			  alert(data.resultado); 
+			  if (data){
+				  if(data.result=="success"){
+					  alert("Processo de refresh do candidato")
+				  }
+				  $("div.messageBox").trigger("bota_e_tira", {"res":data.result, "msg":data.message});
+			  }
 		  },
 		  dataType: "json"
 		});
-
 		
 	});
 	
-	
-	
-	
-	
+	$("div#banner", $("div.container")).trigger("next_pic");
 	
 })
