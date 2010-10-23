91d42d29fc16cfcf1576cca6fe4c377c158a1ecd
Marcelo <marcelosilva@Marcelo-Silvas-Mac.local>
2010-10-23 17:41:32 -0200

    Votando

--- twittface/media/script/voto.js
+++ twittface/media/script/voto.js
@@ -8,7 +8,16 @@ $(document).ready( function(){
 		var candidato = $("input[name='candidato']").val();
 		var url = "/voto?eleitor="+eleitor+"&candidato="+candidato+"&nota="+nota;
 		
-		alert(url)
+		$.ajax({
+		  type: 'POST',
+		  url: url,
+		  data: { "nota": nota, "eleitor": eleitor, "candidato": candidato  },
+		  success: function(data){
+			  alert(data.resultado); 
+		  },
+		  dataType: "json"
+		});
+
 		
 	});
 	
