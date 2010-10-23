1ada3cda573ca6f24c47c5643b7b558c9c5a6452
Claudio Leite <claudio.leite@corp.globo.com>
2010-10-23 17:49:57 -0200

    model de campanha

---
+++ twittface/models/campanha.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+#!/usr/bin/env python
+
+from torneira.models.base import Model, Repository
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.databases.mysql import MSBit
+
+class Campanha (Model, Repository):
+    __tablename__ = 'tface_campanha'
+    
+    id = Column("id", Integer, primary_key=True)
+    nome = Column("nome", String)
+    ativo = Column('ativo', MSBit(1), default=0)
+    votos = Column("votos", Integer)
+    tempo = Column("tempo", Integer)
+    inicio = Column("inicio", DateTime)
